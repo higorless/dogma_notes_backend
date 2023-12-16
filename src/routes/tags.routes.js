928e5372a20ac4722cd1a86f06473cf1dad3fb1a
higor <higor.fernandes98@hotmail.com>
2023-12-16 14:53:51 -0300

    Adding project backend files

---
+++ src/routes/tags.routes.js
@@ -0,0 +1,18 @@
+const { Router } = require("express");
+
+const TagsController = require("../controllers/TagsController");
+const tagsRoutes = Router();
+
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
+
+// function myMiddleware(request, response, next) {
+//   console.log("Você passou pelo MiddleWare");
+
+//   next();
+// }
+
+const tagsController = new TagsController();
+
+tagsRoutes.get("/", ensureAuthenticated, tagsController.index);
+
+module.exports = tagsRoutes;
